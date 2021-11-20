a10d23783f77859cc19dc3f48580109a0fa7436d
Gabriel Siqueira Lannes <85812570+Gasila@users.noreply.github.com>
2021-11-20 18:03:52 -0300

    Add files via upload

---
+++ test01.js
@@ -0,0 +1,13 @@
+function myFunction() {
+
+	var senha = "apoienautilus"
+
+	var chute = prompt("???")
+
+	if(chute == senha){
+  	location.href = "levelum.htm";
+	} else if(chute != senha){
+  	alert("nope!")
+	}
+
+}
